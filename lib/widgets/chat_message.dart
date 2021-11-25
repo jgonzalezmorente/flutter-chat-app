47ab828f95c1fcd6fc6b337106ed236225262e78
José Antonio González Morente <jgonzalezmorente@gmail.com>
2021-11-25 10:29:54 +0100

    Eliminar importación innecesaria en chat_message.dart

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:chat/services/auth_service.dart';
-import 'package:flutter/widgets.dart';
 
 
 class ChatMessage extends StatelessWidget {
